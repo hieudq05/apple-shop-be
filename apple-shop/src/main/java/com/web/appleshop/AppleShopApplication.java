d6e1305e64e043b41ac7da18059c6a5251bd3e13
hieudq05 <hieudqpd10569@gmail.com>
2025-05-28 20:20:20 +0700

    Khởi tạo dự án Spring Boot

---
+++ apple-shop/src/main/java/com/web/appleshop/AppleShopApplication.java
@@ -0,0 +1,13 @@
+package com.web.appleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppleShopApplication.class, args);
+    }
+
+}
---
+++ apple-shop/src/test/java/com/web/appleshop/AppleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.appleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
