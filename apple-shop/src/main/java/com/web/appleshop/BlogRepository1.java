55ea9eeb7fb63f8151059c7f78925cb2787295b8
hieudq05 <hieudqpd10569@gmail.com>
2025-06-07 02:19:23 +0700

    Fix lỗi trước khi push

--- apple-shop/src/main/java/com/web/appleshop/BlogRepository1.java
+++
@@ -1,8 +0,0 @@
-package com.web.appleshop;
-
-import com.web.appleshop.entity.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface BlogRepository1 extends JpaRepository<Blog, Integer>, JpaSpecificationExecutor<Blog> {
-}
